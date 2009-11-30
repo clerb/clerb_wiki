9e215a3e7ed48b5810e538bfe9655eac18966f67
Leon Gersing <fallenrogue@gmail.com>
2009-11-30 16:17:10 -0500

    adding coverage for negative paths scenarios in routes.

--- spec/routing/wiki_routing_spec.rb
+++ spec/routing/wiki_routing_spec.rb
@@ -3,6 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe WikiController, "routing" do
 
   specify { wiki_path("Foo").should == "/wiki/Foo" }
+  
+  specify { wiki_path(WikiDocument.create!).should match /\/wiki\/(\d)+/ }
+  
+  specify { wiki_path(1).should == "/wiki/1" }
 
   specify { route_for(:controller => "wiki",
                       :action => "show",
