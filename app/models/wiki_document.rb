8f5c94a67877a7df54b4ccde45254716f1c9c51e
Jonathan Penn <jonathan@wavethenavel.com>
2010-02-22 19:18:27 -0500

    Document titles are properly validated.

--- app/models/wiki_document.rb
+++ app/models/wiki_document.rb
@@ -1,6 +1,6 @@
 class WikiDocument < ActiveRecord::Base
   validates_presence_of :title
-  validates_format_of :title, :format => /[a-z][A-Z]/
+  validates_format_of :title, :with => /^[A-Z][A-z0-9]*$/
 
   def to_param; title; end
 end
--- spec/models/wiki_document_spec.rb
+++ spec/models/wiki_document_spec.rb
@@ -3,25 +3,44 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe WikiDocument do
 
   before do
-    @wiki_document = WikiDocument.new(:title => "MyDocument", :body => "Lorem ipsum...")
+    @wiki_document = WikiDocument.new(:title => "MyDocument",
+                                      :body => "Lorem ipsum...")
   end
 
   subject { @wiki_document }
 
   it { should be_valid }
-  
-  context "from scratch" do
-    it "should have a title" do
-      wd = WikiDocument.new
-      wd.should_not be_valid
-      wd.title = "Hello World"
-      wd.should be_valid
-    end
-  end
 
   describe "#to_param" do
     it "delegates to title" do
       subject.to_param.should == subject.title
+    end
+  end
+
+  describe "#title" do
+    it "is required" do
+      @wiki_document.title = nil
+      @wiki_document.should_not be_valid
+    end
+
+    it "can contain alpha numeric charaters" do
+      @wiki_document.title = "ABCD1234"
+      @wiki_document.should be_valid
+    end
+
+    it "must start with a capital letter" do
+      @wiki_document.title = "abcd1234"
+      @wiki_document.should_not be_valid
+    end
+
+    it "cannot have punctuation" do
+      @wiki_document.title = "Abcd.1234"
+      @wiki_document.should_not be_valid
+    end
+
+    it "cannot have white space" do
+      @wiki_document.title = "Abcd 1234"
+      @wiki_document.should_not be_valid
     end
   end
 
